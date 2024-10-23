1af340dc4b78d801ffeae4c4f353b6cebb48ed82
chandu9345 <chandrasaitanikonda@gmail.com>
2024-10-23 20:01:22 +0530

    Add files via upload

---
+++ chandu.py
@@ -0,0 +1,29 @@
+list1 = [1,0,1,0,0,1,0,0,0,1]
+a = list1.count(0)
+for i in range (a):
+    list1.remove(0)
+print(list1)
+ la = ["abc", "defg", " higkl"]
+ la.sort(key=None,reverse=False)
+ print(la)
+ def ls(e):
+     return len(e)
+names = ['chandu','karthik','swami']
+names.sort(key=anyfunc)
+print(names)
+
+
+
+l1 = ['1','2','3','4']
+l1.reverse()
+print(l1)
+
+
+l1 = (1,2,3,)
+l1 = l2 
+l1[0]=4
+print(l1)
+
+
+
+    
